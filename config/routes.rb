bb3d6b680a158018b276b494961dbbffe743725c
baqar-abbas <baqar007@hotmail.com>
2023-09-12 23:48:23 +0500

    Created model views and controllers for joins

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :joins
   resources :expenses
   resources :categories
   resources :users
---
+++ test/system/joins_test.rb
@@ -0,0 +1,39 @@
+require "application_system_test_case"
+
+class JoinsTest < ApplicationSystemTestCase
+  setup do
+    @join = joins(:one)
+  end
+
+  test "visiting the index" do
+    visit joins_url
+    assert_selector "h1", text: "Joins"
+  end
+
+  test "should create join" do
+    visit joins_url
+    click_on "New join"
+
+    click_on "Create Join"
+
+    assert_text "Join was successfully created"
+    click_on "Back"
+  end
+
+  test "should update Join" do
+    visit join_url(@join)
+    click_on "Edit this join", match: :first
+
+    click_on "Update Join"
+
+    assert_text "Join was successfully updated"
+    click_on "Back"
+  end
+
+  test "should destroy Join" do
+    visit join_url(@join)
+    click_on "Destroy this join", match: :first
+
+    assert_text "Join was successfully destroyed"
+  end
+end
