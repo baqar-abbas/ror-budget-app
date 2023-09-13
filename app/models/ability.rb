49de194c2c016dee9cd3d4c85e0489f01eb45ef8
baqar-abbas <baqar007@hotmail.com>
2023-09-13 06:50:30 +0500

    Authorization rules defined in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,18 @@
 class Ability
   include CanCan::Ability
 
+  # def initialize(user)
+  #   return unless user.present?
+
+  #   can :manage, Category, user_id: current_user.id
+  #   can :manage, Expense, user_id: current_user.id
+  #   can :manage, User
   def initialize(user)
+    return unless user.present?
+
+    can :manage, Category, user_id: user.id
+    can :manage, Expense, user_id: user.id
+    can :manage, User
     # Define abilities for the user here. For example:
     #
     #   return unless user.present?
