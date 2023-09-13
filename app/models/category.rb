7240fb90723e5a2826c41940ae85c52e6a4c7b7d
baqar-abbas <baqar007@hotmail.com>
2023-09-13 06:38:37 +0500

    Defined associations and validations for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
     belongs_to :user
+    has_many :joins, dependent: :destroy
+    has_many :expenses, through: :joins
+  
+    validates :name, :icon, presence: true, length: { maximum: 50 }
+  
+    def total_expenses(user)
+      expenses.where(user_id: user.id).sum(:amount)
+    end
 end
